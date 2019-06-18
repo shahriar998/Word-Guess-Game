22af003e80e05787cc8318f429eb574ed4f09d35
Shahriar Elias <shahriar998@gmail.com>
2019-06-18 14:49:19 -0400

    loss logic completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,22 +3,20 @@
 
 //variables are declared below
 
-var win =0;
+var win=0;
 var loss=0;
+var guess = 13;
+var noOfBalank;
 var computerWord = ["jeep","gmc","geely","lancia","maserati"]
 var blankArray=[];
-var randomNumber= Math.floor(Math.random()*5);
 var computerRandomWord ="";
+var blankSize;
 //Have to create another variable to break the word into letters
 var wordBreaker=[];
-var numberOfBlank = 0;
 //Array for wrong guess
 var wrongGuess=[];
-var noOfGuesses =13;
-computerRandomWord = computerWord[randomNumber];
-    //test
-console.log(computerRandomWord);
-var wordBreak = [...computerRandomWord];
+
+
 
 
 //********************************************** */
@@ -36,15 +34,29 @@ var htmlNoOfLoss = document.getElementById("loss-count");
 
 
 //logics are below
+//creating a function so that a random word is picked up from the array everytime it's being called and based on that no of blanks will be provided in the display
+function randomWord(){
+    console.log("Start randomWord function")
+    var randomNumber= Math.floor(Math.random()*5);
+    computerRandomWord = computerWord[randomNumber];
+    wordBreaker = [...computerRandomWord];
+    wordBreaker.join(" ");
+    console.log(wordBreaker);
+    for(var i = 0; i<wordBreaker.length;i++){
+        blankArray.push("_");
+
+    }
+    noOfBlank= wordBreaker.length-1;
+    guess=13;
+    console.log("End randomWord function")
+    htmlWordtoGuess.textContent=blankArray.join(" ");
+    
+}
 function gameStart(){
    
-    //test
-    console.log(wordBreak);
-    for(var i = 0; i<wordBreak.length; i++){
-        blankArray.push("_");
-    }
-    //removes "," in the array
-    htmlWordtoGuess.textContent=blankArray.join(" ");
+    randomWord();
+   
+    
 
 }
 
@@ -53,25 +65,50 @@ document.onkeypress= function(event){
     var keyPressed = String.fromCharCode(event.keyCode).toLowerCase();
     //test
     console.log(keyPressed);
+    var keyPressArrayNumberCheck = wordBreaker.indexOf(keyPressed);
+    console.log(keyPressArrayNumberCheck);
 
-    //test
-    console.log(wordBreak);
-
-    function checkLetter(letter){
-        
-        if(wordBreak.indexOf(keyPressed)>-1){
-            for(var i = 0; i<wordBreak.length; i++){
-                if(wordBreak[i]==keyPressed){
-                    blankArray[i]==keyPressed;
-                }
+    if(keyPressArrayNumberCheck!=-1){
+        console.log("I'm here")
+        console.log(blankArray);
+        console.log(wordBreaker);
+        for(var i =0; i<wordBreaker.length; i++){
+            if(keyPressArrayNumberCheck!=1){
+                blankArray[i]=keyPressed;
+                console.log("Boom "+blankArray[i]);
             }
+                
         }
-        else{
-            wrongGuess.push(keyPressed).join(" ");
-
-
+        console.log(blankArray);
+    }
+    else{
+        guess--;
+        htmlNoOfGuess.textContent=guess;
+        if (guess==0){
+            loss++;
+            htmlNoOfLoss.textContent=loss;
+            htmlNoOfGuess.textContent=13;
+            blankArray=[];
+            randomWord();
+            
         }
     }
 
-    checkLetter(keyPressed);
+    // function checkLetter(letter){
+        
+    //     if(wordBreak.indexOf(keyPressed)>-1){
+    //         for(var i = 0; i<wordBreak.length; i++){
+    //             if(wordBreak[i]==keyPressed){
+    //                 blankArray[i]==keyPressed;
+    //             }
+    //         }
+    //     }
+    //     else{
+    //         wrongGuess.push(keyPressed).join(" ");
+
+
+    //     }
+    // }
+
+    // checkLetter(keyPressed);
 }
