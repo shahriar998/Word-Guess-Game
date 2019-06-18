d1568b0dca3ebc90fb4465c90387601db4162a4e
Shahriar Elias <shahriar998@gmail.com>
2019-06-17 23:17:21 -0400

    basic logic in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,35 @@
 //Connection check
 //console.log("Hello")
+
+//variables are declared below
+
+var win =0;
+var loss=0;
+var computerWord = ["jeep","gmc","geely","lancia","maserati"]
+var blankArray=[];
+var randomNumber= Math.floor(Math.random()*5);
+//Have to create another variable to break the word into letters
+var wordBreaker=[];
+
+
+
+//********************************************** */
+
+
+//Elements from HTML declared below 
+
+var htmlWordtoGuess = document.getElementById("word-blank");
+var htmlWrongGuess = document.getElementById("wrong-guess");
+var htmlNoOfGuess = document.getElementById("guess-remaining");
+var htmlNoOfWin = document.getElementById("win-count");
+var htmlNoOfLoss = document.getElementById("loss-count");
+//********************************************** */
+//random word generator below 
+
+
+//logics are below
+document.onkeypress= function(event){
+    var keyPressed = event.key;
+    //test
+    console.log(keyPressed);
+}
